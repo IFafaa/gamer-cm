f172625627d462801abca52e908ba1f44143666e
Fabricio Lima <fabriciotrab2005@gmail.com>
2025-05-17 00:01:52 -0300

    refac: refac end party use case

--- src/application/use_cases/end_party_use_case.rs
+++ src/application/use_cases/end_party_use_case.rs
@@ -30,31 +30,33 @@ impl<PR: PartyRepository, TR: TeamRepository> EndPartyUseCase<PR, TR> {
                     StatusCode::INTERNAL_SERVER_ERROR,
                     ApiErrorResponse::new("Failed to fetch party".to_string()),
                 )
-            })?;
-
-        if party.is_none() {
-            return Err((
+            })?
+            .ok_or((
                 StatusCode::BAD_REQUEST,
                 ApiErrorResponse::new("Party not found".to_string()),
-            ));
-        }
+            ))?;
 
-        let team_winner = if let Some(team_id) = dto.team_winner_id {
-            self.team_repository.get_by_id(team_id).await.map_err(|_| {
-                (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    ApiErrorResponse::new("Failed to fetch team".to_string()),
-                )
-            })?
-        } else {
-            None
+        let team_winner = match dto.team_winner_id {
+            Some(team_id) => Some(
+                self.team_repository
+                    .get_by_id(team_id)
+                    .await
+                    .map_err(|_| {
+                        (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            ApiErrorResponse::new("Failed to fetch team".to_string()),
+                        )
+                    })?
+                    .ok_or((
+                        StatusCode::BAD_REQUEST,
+                        ApiErrorResponse::new("Team not found".to_string()),
+                    ))?,
+            ),
+            None => None,
         };
 
-        let mut party = party.unwrap();
-
-        if let Some(team) = &team_winner {
+        if let Some(ref team) = team_winner {
             let is_team_not_in_party = !party.teams.iter().any(|t| t.id == team.id);
-
             if is_team_not_in_party {
                 return Err((
                     StatusCode::BAD_REQUEST,
@@ -63,7 +65,9 @@ impl<PR: PartyRepository, TR: TeamRepository> EndPartyUseCase<PR, TR> {
             }
         }
 
-        party.end(dto.team_winner_id);
+        let mut party = party;
+        let winner_id = team_winner.map(|team| team.id);
+        party.end(winner_id);
 
         self.party_repository.save(&party).await.map_err(|_| {
             (
