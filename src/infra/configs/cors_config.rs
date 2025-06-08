5fcd6cbdeaf2d21f4c11222e04885b74ae5fb264
Fabricio Lima <fabriciotrab2005@gmail.com>
2025-06-07 23:48:47 -0300

    chore: enable all methods cors

--- src/infra/configs/cors_config.rs
+++ src/infra/configs/cors_config.rs
@@ -4,13 +4,7 @@ use tower_http::cors::{Any, CorsLayer};
 pub fn cors_config() -> Result<CorsLayer, Box<dyn std::error::Error>> {
     let cors = CorsLayer::new()
         .allow_origin(Any)
-        .allow_methods([
-            http::Method::GET,
-            http::Method::POST,
-            http::Method::PUT,
-            http::Method::DELETE,
-            http::Method::PATCH,
-        ])
+        .allow_methods(Any)
         .allow_headers([http::header::CONTENT_TYPE]);
     Ok(cors)
 }
